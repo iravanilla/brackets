9d505890fd5c1a41fa263bd9cab3b0974fe88941
iravanilla <i.krzh@yandex.ru>
2020-10-11 21:10:09 +0400

    feat: solution of the 'brackets' problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (stack.length === 0) {
+            stack.push(str[i]);
+            continue;
+        } 
+
+        let hasFound = false;
+        for(let k = 0; k < bracketsConfig.length; k++) {
+            if (stack[stack.length-1] === bracketsConfig[k][0] && str[i] === bracketsConfig[k][1]) {
+                stack.pop();
+                hasFound = true;
+                break;
+            } 
+        }
+        if(!hasFound) {
+            stack.push(str[i]);
+        }
+    }
+
+    return stack.length === 0;
+
 }
+
